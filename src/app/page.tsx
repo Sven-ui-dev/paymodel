4cfe4485a1cd0b39b7afc632ff39c11363564208
Sven Grewe <svengrewe@openclaw.fritz.box>
2026-02-18 16:19:08 +0100

    fix: import toast from sonner package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { Toaster, toast } from "@/components/ui/sonner";
+import { Toaster, toast } from "sonner";
 import { ModelList } from "@/components/ModelList";
 import { PriceCalculator } from "@/components/PriceCalculator";
 import { SearchFilter } from "@/components/SearchFilter";
